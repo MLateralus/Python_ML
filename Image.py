84a47f044a33c0b7afaae98b81e631355078c5d2
Lateralus <michal.czerwien@gmail.com>
2017-07-06 13:09:07 +0200

    params recalculation

--- Image.py
+++ Image.py
@@ -1,6 +1,7 @@
 from scipy import misc
 import matplotlib.pyplot as plt
-from skimage.restoration import denoise_nl_means, denoise_tv_chambolle
+from scipy.signal import convolve2d
+from skimage.restoration import denoise_nl_means, denoise_tv_chambolle, estimate_sigma
 
 
 class Image:
@@ -12,10 +13,10 @@ class Image:
         self.interpol_num = len(interpolations)
 
     def draw(self):
-        fig, ax = plt.subplots(nrows=3, ncols=3)
+        fig, ax = plt.subplots(nrows=2, ncols=3)
 
         # Reference
-        plt.subplot(3, 3, 1)
+        plt.subplot(2, 3, 1)
         plt.imshow(self.image, interpolation="None")
         plt.ylabel("Reference")
 
@@ -35,24 +36,23 @@ class Image:
     def imshow_interpols(self):
         for i in range(0, self.interpol_num):
             interpol = self.interpolations[i]
-            plt.subplot(3, 3, i + 2)
-            # plt.imshow(bait[130:180, 130:170], interpolation=interpol)
-            plt.imshow(self.image, interpolation=interpol)
+            plt.subplot(2, 3, i + 2)
+            plt.imshow(self.image[90:250, 80:260], interpolation=interpol)
             plt.ylabel(self.interpolations[i])
             self.__log__(i, interpol)
 
     def nl_means(self):
         self.__log__("Denoising nl means.....")
-        plt.subplot(3, 3, 5)
-        denoise = denoise_nl_means(self.image, 7, 9, 0.1, multichannel=True)
+        plt.subplot(2, 3, 5)
+        denoise = denoise_nl_means(self.image[90:250, 80:260], 7, 9, 0.1, multichannel=True)
         plt.imshow(denoise)
         plt.ylabel("Non_local means")
         self.__log__("Denoising nl means done")
 
     def total_variations(self):
         self.__log__("Denoising total variations.....")
-        plt.subplot(3, 3, 6)
-        tv_denoised = denoise_tv_chambolle(self.image, weight=5)
+        plt.subplot(2, 3, 6)
+        tv_denoised = denoise_tv_chambolle(self.image[90:250, 80:260], weight=0.1, multichannel=True)
         plt.imshow(tv_denoised)
         plt.ylabel("Total Variation denoise")
         self.__log__("Denoising total variations done")
@@ -63,6 +63,6 @@ class Image:
 
 if __name__ == '__main__':
 
-    G = Image("C:/Users/Lateralus/Pictures/Peny.png", ['nearest', 'bilinear', 'bessel', 'quadric'], 4)
-    G.draw()
+    newImage = Image("C:/Users/Lateralus/Pictures/Peny.png", ['nearest', 'bilinear', 'quadric'], 4)
+    newImage.draw()
 
